a52b247fba36c706d0e6558a16d1b178b3038e62
MuskChan <523919565@qq.com>
2019-08-11 17:34:29 +0800

    init

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('login', 'LoginController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
